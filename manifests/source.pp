879a4dee5b16768b03e7c127f966ad499a4c255e
JEarl <jeffrey.earl@jetblue.com>
2016-09-28 11:37:31 -0400

    patched source.pp to work with puppet/archive

--- manifests/source.pp
+++ manifests/source.pp
@@ -11,7 +11,8 @@
 #
 # Requires:
 # - java to be previously installed
-# - archive definition (from puppet camptocamp/puppet-archive module)
+# JB REMOVED Requirement: - archive definition (from puppet camptocamp/puppet-archive module)
+# JB MODIFIED - Patched to use puppet/archive rather than camptocamp/puppet-archive module
 # - Package["curl"]
 #
 # Tested on:
@@ -41,23 +42,36 @@ class tomcat::source {
   $baseurl = "${sources_src}/tomcat-${maj_version}/v${version}/bin"
   $tomcaturl = "${baseurl}/apache-tomcat-${version}.tar.gz"
 
-  archive{ "apache-tomcat-${version}":
-    url         => $tomcaturl,
-    digest_url  => "${tomcaturl}.md5",
-    digest_type => 'md5',
-    target      => '/opt',
+#  archive{ "apache-tomcat-${version}":
+#    url         => $tomcaturl,
+#    digest_url  => "${tomcaturl}.md5",
+#    digest_type => 'md5',
+#    target      => '/opt',
+#  }
+
+# JB - Modified for compatability with puppet/archive module
+  archive{ "apache-tomcat-${version}.tar.gz":
+    path          => "/tmp/apache-tomcat-${version}.tar.gz",
+    source        => $tomcaturl,
+    checksum_url  => "${tomcaturl}.md5",
+    checksum_type => 'md5',
+    extract       => true,
+    extract_path  => '/opt',
+    creates       => "/opt/apache-tomcat-${version}",
+    cleanup       => true,
   }
+
 
   file { '/opt/apache-tomcat':
     ensure  => link,
     target  => $::tomcat::home,
-    require => Archive["apache-tomcat-${version}"],
+    require => Archive["apache-tomcat-${version}.tar.gz"],
     before  => Class['tomcat::logging'],
   }
 
   file { $::tomcat::home:
     ensure  => directory,
-    require => Archive["apache-tomcat-${version}"],
+    require => Archive["apache-tomcat-${version}.tar.gz"],
   }
 
   # Workarounds
@@ -67,7 +81,7 @@ class tomcat::source {
       file {"${::tomcat::home}/bin/catalina.sh":
         ensure  => file,
         content => file(sprintf('%s/files/catalina.sh-6.0.18', get_module_path($module_name))),
-        require => Archive["apache-tomcat-${tomcat::version}"],
+        require => Archive["apache-tomcat-${tomcat::version}.tar.gz"],
         mode    => '0755',
       }
     }
